109de551f3fdf2b54e8ce9f39b32a9db985fdf9c
Azul <azul@riseup.net>
2012-03-14 10:10:13 +0100

    get the pad name and the server name from the index

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ require(['pad', 'jquery'], function (pad, $) {
   $(document).ready(function()
     {
       // require('/pad').getParams();
-      pad.handshake('http://surf.unhosted.org:82','socket.io', 'azul@5apps.com$testing-things');
+      var server = localStorage.etherpadServer;
+      var padName = localStorage.padName;
+      // set some defaults - http port 80
+      // TODO: use url for this.
+      if(server.indexOf('http') != 0) server = "http://" + server;
+      if(server.lastIndexOf(':') < 7) server = server + ':80';
+      pad.handshake(server, 'socket.io', padName);
     });
 
   /* TODO: These globals shouldn't exist. */
